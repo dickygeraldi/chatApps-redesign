1ebf0caa1cc72ce43279563c8601940702f7f122
dickygeraldi <dickygeraldi@gmail.com>
2020-04-29 17:42:13 +0700

    sada

--- chatApps_redesign/Views/GlobalFunction.swift
+++ chatApps_redesign/Views/GlobalFunction.swift
@@ -7,3 +7,17 @@
 //
 
 import Foundation
+
+func getDate() -> String {
+    let date = Date()
+    let calendar = Calendar.current
+    let hour = calendar.component(.hour, from: date)
+    let minutes = calendar.component(.minute, from: date)
+    
+    return "\(hour):\(minutes)"
+}
+
+func randomString(length: Int) -> String {
+    let letters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+    return String((0..<length).map{ _ in letters.randomElement()! })
+}
